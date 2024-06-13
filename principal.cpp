206a9607edc3039a4c7f2b0c248c31cc06f1848c
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2024-06-13 01:36:58 -0600

    sh

---
+++ principal.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+// Lista de programas
+//		1.- GaussJordan.cpp (gauss_jordan_p1)
+//		2.- Gauss.cpp (gauss_p2)
+//		3.- Gauss-Seidel.cpp (gauss_seidel_p3)
+//		4.- Interpolacion Cuadratica de Newton.cpp (interpolacion_cuadratica_newton_p4)
+//		5.- Interpolacion lineal.cpp (interpolacion_lineal_p5)
+//		6.- Interpolacion n=3.cpp (interpolacion_n3_p6)
+//		7.- Langrage.cpp (langrage_p7)
+//		8.- Metodo de Biseccion.cpp (metodo_biseccion_p8)
+//		9.- Metodo de la Secante.cpp (metodo_secante_p9)
+//		10.- Metodo de la Tangente.cpp (metodo_tangente_p10)
+//		11.- Metodo del Trapecio.cpp (metodo_trapecio_p11)
+//		12.- Minimos Cuadrados (minimos_cuadrados_p12)
+//		13.- 
+//		14.- 
+
+int main() {
+
+}
